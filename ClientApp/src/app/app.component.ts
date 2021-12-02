8d73d249b8307af969ff5c65231e637922494580
Daviti-Gogniashvili <gogniashvili.dg@gmail.com>
2021-12-02 22:30:12 +0400

    Update of phase 3

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ClientApp';
+export class AppComponent implements OnInit {
+  title = 'Dating App';
+  public displayedColumns: string[] = ['id', 'userName'];
+  public users: Users[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Users[]>('https://localhost:5001/API/AppUsers').subscribe(result => {
+      this.users = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Users {
+  id: number;
+  userName: string;
 }
